cb7568ae756a3cff6ac97831f46d8f1fb56a27c3
Carlos Franco <franker5588@gmail.com>
2021-11-17 23:35:30 -0400

    Otra vez

--- post.php
+++ post.php
@@ -1,18 +1,22 @@
 <?php
+        $to = "franker5588@gmail.com";
 
-        $name = $_POST[`name`];
-        $email = $_POST[`email`];
-        $message = $_POST[`message`];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
         $header = "From:". $email . "\r\n";
         
-        $msg = "Mensaje es de: ". $name . "\r\n";
+        $msg = "Mensaje de: ". $name . "\r\n";
         $msg.= "Email: ". $email . "\r\n";
         $msg.= "Su mensaje es: ". $message;
         
-        $to = "franker5588@gmail.com";
         $issue = "Mensaje desde mi página";
 
-        mail($to, $issue, $msg, $header);
-        echo '<script>alert("Correo enviado exitosamente)</script>';
+        if(mail($to, $issue, $msg, $header)){
+            echo '<script>alert("Correo enviado exitosamente")</script>';
+        }else{
+            echo '<script>alert("fallo de envio")</script>';
+        }
 
+        echo "<script>setTimeOut(.\"location.href='index.html'\",1000)</script>";
 ?>
