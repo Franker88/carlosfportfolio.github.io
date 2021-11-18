304961d008ff160925be0698e6e5ad8b77ab2381
Carlos Franco <franker5588@gmail.com>
2021-11-17 22:41:35 -0400

    New Correct

--- main.js
+++ main.js
@@ -5,43 +5,28 @@
 /*Linked In*/
 let ldin = document.getElementById("linkedin");
 
-ldin.addEventListener("mouseover",function(){
-    document.getElementById("contactn").innerHTML = "Carlos Franco";
-})
-
-ldin.addEventListener("mouseout",function(){
-    document.getElementById("contactn").innerHTML = "";
+ldin.addEventListener("click",function(){
+    alert("LinkedIn: Carlos Franco");
 })
 
 /*Git-Hub*/
 let gh = document.getElementById("github");
 
-gh.addEventListener("mouseover",function(){
-    document.getElementById("contactn").innerHTML = "Franker88";
-})
-
-gh.addEventListener("mouseout",function(){
-    document.getElementById("contactn").innerHTML = "";
+gh.addEventListener("click",function(){
+    alert("GitHub: Franker88");
 })
 
 /*Instagram*/
 let ins = document.getElementById("instagram");
 
-ins.addEventListener("mouseover",function(){
-    document.getElementById("contactn").innerHTML = "@franco.c.v";
-})
-
-ins.addEventListener("mouseout",function(){
-    document.getElementById("contactn").innerHTML = "";
+ins.addEventListener("click",function(){
+    alert("Instagram: franco.c.v");
 })
 
 /*Whatsapp*/
 let wap = document.getElementById("whatsapp");
 
-wap.addEventListener("mouseover",function(){
-    document.getElementById("contactn").innerHTML = "+58 414 900 8364";
+wap.addEventListener("click",function(){
+    alert("Whatsapp: +58 414 900 83 64")
 })
 
-wap.addEventListener("mouseout",function(){
-    document.getElementById("contactn").innerHTML = "";
-})
--- post.php
+++ post.php
@@ -6,18 +6,15 @@ if (isset($_POST['enviar'])) {
         $email = $_POST[`email`];
         $message = $_POST[`message`];
         $header = "From:". $email . "\r\n";
-        $header.= "X-Mailer: PHP/". phpversion() . "\r\n";
         
-        $msg = "El mensaje es de: ". $name . "\r\n";
+        $msg = "Mensaje es de: ". $name . "\r\n";
         $msg.= "Email: ". $email . "\r\n";
         $msg.= "Su mensaje es: ". $message;
         
         $to = "franker5588@gmail.com";
         $issue = "Mensaje desde mi página";
 
-        $mail = mail($to,$issue, utf8_decode($msg),$header);
-        if ($mail) {
-            echo "<h4>¡Enviado exitosamente!</h4>";
-        }
+        mail($to, $issue, $msg, $header);
+        echo '<script>alert("Correo enviado exitosamente)</script>';
     }
 }
